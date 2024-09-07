ae4b808b818e2a2f5191cf0b6adc3ffa5da4c0b2
AdieDev0 <devutella@gmail.com>
2024-09-07 18:46:56 +0800

    tite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Tite ko</h1>
+      <h2>Tite</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
