f3dd888e04654ae1c2e96a4d9222e1f8c6ab5292
japhethh <boknoy32130@gmail.com>
2024-09-07 18:49:57 +0800

    New commit -japheth

--- frontend/src/Components/Jappp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Jappp = () => {
-  return (
-    <div>Japppppp</div>
-  )
-}
-
-export default Jappp
--- frontend/src/Components/Yo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Yo = () => {
-  return (
-    <div>tite</div>
-  )
-}
-
-export default Yo
